04fd34e98e1798c87e8cdaedb825c41627a9e2f0
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-07-08 20:23:42 +0200

    added disablingInput, countingCurrency, input, EUR

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,63 @@
-console.log("Cześć")
-//variables
-let cantor = document.querySelector(".js-form")
-let priceTop = document.querySelector(".js-price1");
-let priceBottom = document.querySelector(".js-price2");
-let changeCurrency = document.querySelector(".js-changeIcon");
-let currencyTop = document.querySelector(".js-currency1");
-let currencyBottom = document.querySelector(".js-currency2");
-let currencyEuro = ((Math.random()/5) + 4.3).toFixed(4);
-let currencyPLN = (1 / currencyEuro).toFixed(4);
-let euro = document.querySelector(".euro");
-let pln = document.querySelector(".pln");
+{
+    //variables
+    const topValue = document.querySelector(".js-price1");
+    const bottomValue = (document.querySelector(".js-price2"));
+    const changeCurrencyButton = document.querySelector(".js-changeIcon");
+
+    const currencyEuro = ((Math.random() / 5) + 4.3).toFixed(4);
+    const currencyPLN = (1 / currencyEuro).toFixed(4);
+    const currencyTopSign = document.querySelector(".js-currencyTop");
+    const currencyBottomSign = document.querySelector(".js-currencyBottom");
 
 
-currencyTop.innerHTML = currencyEuro;
-currencyBottom.innerHTML = currencyPLN;
-//disabling the second input while filling in the first
-priceTop.addEventListener("input", () => {
-    priceBottom.setAttribute("disabled", "")
-})
-//counting the currency
-cantor.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let currencyResult = undefined;
-    if (euro.innerText === "€") {
-        currencyResult = +priceTop.value * currencyEuro;
-    } else {
-        currencyResult = +priceTop.value * currencyPLN;
+
+
+    const disablingInput = () => {
+        bottomValue.setAttribute("disabled", "")
     }
-    priceBottom.value = currencyResult.toFixed(2);
-});
-//action after clicking on arrows
-changeCurrency.addEventListener("click", () => {
-    if (euro.innerText === "€") {
-        euro.innerText = "PLN";
-    } //doesn't work with &euro;, dunno why
-    else {
-        euro.innerText = "€";
-    };
-    if (pln.innerText === "PLN") {
-        pln.innerText = "€";
-    } else {
-        pln.innerText = "PLN";
-    };
-    priceTop.value = "";
-    priceBottom.value = "";
-});
+    const countingCurrency = () => {
+        let currencyResult;
+        if (currencyTopSign.innerText === "EUR") {
+            currencyResult = +topValue.value * currencyEuro;
+        } else {
+            currencyResult = +topValue.value * currencyPLN;
+        }
+        bottomValue.value = currencyResult.toFixed(2);
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        const currencyTop = document.querySelector(".js-currency1");
+        const currencyBottom = document.querySelector(".js-currency2");
+
+        currencyTop.innerHTML = currencyEuro;
+        currencyBottom.innerHTML = currencyPLN;
+
+        topValue.addEventListener("input", disablingInput);
+        formElement.addEventListener("submit", (e) => {
+            e.preventDefault();
+            countingCurrency();
+        });
+    }
+    init()
+
+
+
+    //action after clicking on arrows
+    changeCurrencyButton.addEventListener("click", () => {
+        if (currencyTopSign.innerText === "EUR") {
+            currencyTopSign.innerText = "PLN";
+        } else {
+            currencyTopSign.innerText = "EUR";
+        };
+        if (currencyBottomSign.innerText === "PLN") {
+            currencyBottomSign.innerText = "EUR";
+        } else {
+            currencyBottomSign.innerText = "PLN";
+        };
+        topValue.value = "";
+        bottomValue.value = "";
+    });
+
+    
+}
