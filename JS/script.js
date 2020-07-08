8fb505a4b6120a9d28152693261aa974260baf5b
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-07-08 20:30:25 +0200

    added toggleCurrencies

--- JS/script.js
+++ JS/script.js
@@ -2,12 +2,10 @@
     //variables
     const topValue = document.querySelector(".js-price1");
     const bottomValue = (document.querySelector(".js-price2"));
-    const changeCurrencyButton = document.querySelector(".js-changeIcon");
 
     const currencyEuro = ((Math.random() / 5) + 4.3).toFixed(4);
     const currencyPLN = (1 / currencyEuro).toFixed(4);
     const currencyTopSign = document.querySelector(".js-currencyTop");
-    const currencyBottomSign = document.querySelector(".js-currencyBottom");
 
 
 
@@ -24,27 +22,9 @@
         }
         bottomValue.value = currencyResult.toFixed(2);
     }
+    const toggleCurrencies = () => {
+        const currencyBottomSign = document.querySelector(".js-currencyBottom");
 
-    const init = () => {
-        const formElement = document.querySelector(".js-form");
-        const currencyTop = document.querySelector(".js-currency1");
-        const currencyBottom = document.querySelector(".js-currency2");
-
-        currencyTop.innerHTML = currencyEuro;
-        currencyBottom.innerHTML = currencyPLN;
-
-        topValue.addEventListener("input", disablingInput);
-        formElement.addEventListener("submit", (e) => {
-            e.preventDefault();
-            countingCurrency();
-        });
-    }
-    init()
-
-
-
-    //action after clicking on arrows
-    changeCurrencyButton.addEventListener("click", () => {
         if (currencyTopSign.innerText === "EUR") {
             currencyTopSign.innerText = "PLN";
         } else {
@@ -57,7 +37,24 @@
         };
         topValue.value = "";
         bottomValue.value = "";
-    });
+    }
 
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        const currencyTop = document.querySelector(".js-currency1");
+        const currencyBottom = document.querySelector(".js-currency2");
+        const changeCurrencyButton = document.querySelector(".js-changeIcon");
+
+        currencyTop.innerHTML = currencyEuro;
+        currencyBottom.innerHTML = currencyPLN;
+
+        topValue.addEventListener("input", disablingInput);
+        formElement.addEventListener("submit", (e) => {
+            e.preventDefault();
+            countingCurrency();
+        });
+        changeCurrencyButton.addEventListener("click", toggleCurrencies);
+    }
+    init()
     
 }
